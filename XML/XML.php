8e376d70e7e6c908dd879a29791c2d38127cfd4a
Travis van der Font <travis.font@gmail.com>
2017-06-26 18:24:50 +0200

    add XML library

---
+++ XML/XML.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace
+{
+    final class XML
+    {
+        /**
+         * Substitute entities
+         *
+         * @link http://php.net/manual/en/libxml.constants.php
+         */
+        const PARSE_EXPAND_ENTITIES = \LIBXML_NOENT;
+    }
+}
